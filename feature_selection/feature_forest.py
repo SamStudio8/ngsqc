cd6e33b800ee6b9190399f8867d4604708eebf22
Sam Nicholls <sam.n@studio8media.co.uk>
2014-08-18 18:22:15 +0100

    Extract plot_tree to util

--- feature_selection/feature_forest.py
+++ feature_selection/feature_forest.py
@@ -58,20 +58,6 @@ def setup():
 def iterate():
     pass
 
-def plot_tree(pdf_filename, clf):
-    try:
-        import pydot
-    except ImportError, e:
-        print("Please install 'pydot', it will require Graphviz.")
-        sys.exit(1)
-
-    from sklearn.externals.six import StringIO
-
-    dot_data = StringIO()
-    export_graphviz(clf, out_file=dot_data)
-    graph = pydot.graph_from_dot_data(dot_data.getvalue())
-    graph.write_pdf(pdf_filename)
-
 statplexer = setup()
 all_parameters = statplexer.exclude_parameters(NO_VARIANCE + RAW_COUNTS)
 data, target, levels = statplexer.get_data_by_target(all_parameters, USE_TARGETS)
---
+++ feature_selection/util.py
@@ -0,0 +1,14 @@
+def plot_tree(pdf_filename, clf):
+    try:
+        import pydot
+    except ImportError, e:
+        print("Please install 'pydot', it will require Graphviz.")
+        sys.exit(1)
+
+    from sklearn.externals.six import StringIO
+
+    dot_data = StringIO()
+    export_graphviz(clf, out_file=dot_data)
+    graph = pydot.graph_from_dot_data(dot_data.getvalue())
+    graph.write_pdf(pdf_filename)
+
